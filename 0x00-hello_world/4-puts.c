51069bf9327219dad27f8e675327e1ba52c08a7a
alpha-271 <iabdulmuiz101@gmail.com>
2023-02-16 16:58:19 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- A progrm that shows -o with puts
+ * Return: return (0)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
