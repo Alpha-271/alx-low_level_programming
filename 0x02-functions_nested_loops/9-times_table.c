f61c0efb09949485ec16a619fc0b603ff0b0ee4a
alpha-271 <iabdulmuiz101@gmail.com>
2023-02-21 19:43:07 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,39 @@
 #include "main.h"
 /**
+ * times_table - print table 9
  *
- *
- *
+ * Return: returns nothing
  */
 
 void times_table(void)
 {
-	int i;
-	int x;
+	int x, y, z, u, d;
 
-	for (i = 0; i < 10 i++)
+	for (x = 0; x <= 9; x++)
 	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z  > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
