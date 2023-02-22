9337c8c43a109866b56bdfe3d17fe21f2daae4d7
alpha-271 <iabdulmuiz101@gmail.com>
2023-02-21 18:14:01 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - a function that adds two numbers
+ * @i: stores first input
+ * @x: stores second input
+ * Return: returns the result
+ */
+
+int add(int i, int x)
+{
+	int n;
+
+	n = i + x;
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 10 i++)
+	{
