b97e991a769a5130aa6dc7cde3c384149eec5516
alpha-271 <iabdulmuiz101@gmail.com>
2023-02-21 17:51:09 -0800

    24 hrs time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints time
+ *
+ * Return: returns nothing
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (x = 0; x < 60; x++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
