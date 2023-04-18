0da1c6d1622c7496a690d3a91ea8fe7b08373bd3
alpha-271 <iabdulmuiz101@gmail.com>
2023-04-17 19:33:03 -0700

    correcting error in project 2 and 1:

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,34 @@
 #include "main.h"
 
 /**
- * create_file - Makes a new file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string of chars to input to the file.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: If the function fails  -1.
- *         Otherwise 1.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int x;
-	int y;
-	int length;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length; text_content[length];)
-			length++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	y = write(x, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (x == -1 || y == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(x);
+	close(o);
 
 	return (1);
 }
